5228bdceed17205b6f42c221a083840bab7aaf7e
Bikkul <quadalus@yandex.ru>
2022-10-26 16:57:49 +0300

    feat: намечена базовая функциональность

---
+++ src/main/java/ru/yandex/practicum/filmrate/FilmrateApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.practicum.filmrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmrateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmrateApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ru/yandex/practicum/filmrate/model/Film.java
@@ -0,0 +1,9 @@
+package ru.yandex.practicum.filmrate.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class Film {
+}
