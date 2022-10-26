f0c66fb8765e5a4a032e69d882a30253048eb678
Bikkul <quadalus@yandex.ru>
2022-10-26 17:31:53 +0300

    feat: create model entity

--- src/main/java/ru/yandex/practicum/filmrate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmrate/model/Film.java
@@ -2,8 +2,17 @@ package ru.yandex.practicum.filmrate.model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
 
-@Builder
+import java.time.LocalDate;
+
 @Data
+@Builder
 public class Film {
+    private int id;
+    private String name;
+    private String description;
+    @NonNull
+    private LocalDate releaseDate;
+    private long duration;
 }
--- src/main/java/ru/yandex/practicum/filmrate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmrate/model/User.java
@@ -2,8 +2,17 @@ package ru.yandex.practicum.filmrate.model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NonNull;
 
-@Builder
+import java.time.LocalDate;
+
 @Data
+@Builder
 public class User {
+    private int id;
+    private String email;
+    private String login;
+    private String name;
+    @NonNull
+    private LocalDate birthday;
 }
