7797601453dd483773d8e1c355e5a2a47aa38b46
Bikkul <quadalus@yandex.ru>
2022-11-19 01:18:28 +0300

    feat: add exception handlers

--- src/main/java/ru/yandex/practicum/filmrate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmrate/controller/ErrorHandler.java
@@ -1,7 +1,30 @@
 package ru.yandex.practicum.filmrate.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.yandex.practicum.filmrate.exception.NotFoundException;
+import ru.yandex.practicum.filmrate.exception.ValidationException;
+import ru.yandex.practicum.filmrate.model.ErrorResponse;
 
-@RestControllerAdvice
+@RestControllerAdvice("ru.yandex.practicum.controller")
 public class ErrorHandler {
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleValidationException(final ValidationException e) {
+        return new ErrorResponse("Ошибка валидации!", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorResponse handleNotFoundException(final NotFoundException e) {
+        return new ErrorResponse("Запрошенные данные не найдены!", e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorResponse handleInternalException(final InternalError e) {
+        return new ErrorResponse("Неизвестная ошибка сервера!", e.getMessage());
+    }
 }
--- src/main/java/ru/yandex/practicum/filmrate/model/ErrorResponse.java
+++ src/main/java/ru/yandex/practicum/filmrate/model/ErrorResponse.java
@@ -7,4 +7,5 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public final class ErrorResponse {
     private final String error;
+    private final String description;
 }
