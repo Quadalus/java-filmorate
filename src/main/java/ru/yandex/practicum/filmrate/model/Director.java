1637b6e578407191b033d1d82938573d7fe2ef69
valentinakolesnikova <polikarpova-v@yandex.ru>
2022-12-20 22:32:26 +0300

    feat: добавлены модельные классы для режиссера и режиссеры/фильмы

---
+++ src/main/java/ru/yandex/practicum/filmrate/model/Director.java
@@ -0,0 +1,15 @@
+package ru.yandex.practicum.filmrate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Director {
+	private Integer id;
+	private String name;
+}
