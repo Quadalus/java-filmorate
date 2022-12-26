e60786b8bc72364799c45798e6389704a05bdaf2
Valentina <83814517+Valentina810@users.noreply.github.com>
2022-12-26 14:00:22 +0300

    Add feed (#14)
    * feat: добавить функциональность Лента событий
    
    * feat: вынести заполнение справочников в конструктор
    
    * fix: убрать FeedController, метод для получения ленты перенести в UserController
    
    * fix: поменять тип id пользователя в методе getUserEvents на int, мелкие правки по форматированию
    
    * fix: поправить тесты
    
    * fix: поправить тесты

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -47,8 +47,7 @@ CREATE TABLE IF NOT EXISTS films_genres
 CREATE TABLE IF NOT EXISTS likes
 (
     film_id INT REFERENCES films (film_id) ON DELETE CASCADE,
-    user_id INT REFERENCES users (user_id) ON DELETE CASCADE,
-    PRIMARY KEY (film_id, user_id)
+    user_id INT REFERENCES users (user_id) ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS reviews
